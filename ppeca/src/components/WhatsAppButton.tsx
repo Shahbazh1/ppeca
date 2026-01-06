6b0e9d49b713bde16f8890c22c2dbb1bb6457b42
Shahbazh1 <shahbazhashmi235@gmail.com>
2026-01-06 23:00:01 +0500

    fixed

--- ppeca/src/components/WhatsAppButton.tsx
+++ ppeca/src/components/WhatsAppButton.tsx
@@ -14,7 +14,7 @@ export default function WhatsAppButton() {
       aria-label="Chat on WhatsApp"
       className="
         fixed bottom-5 right-5 z-[9999]
-        bg-green-500 hover:bg-green-600
+        bg-[#14a637] hover:bg-green-600
         p-3 rounded-full
         shadow-md
         transition-all duration-300
