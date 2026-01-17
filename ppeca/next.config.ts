fb76062c5de25f5026cb4eb5ac7d78b6519b0e43
Shahbazh1 <shahbazhashmi235@gmail.com>
2026-01-18 04:58:39 +0500

    fixed redirect url

--- ppeca/next.config.ts
+++ ppeca/next.config.ts
@@ -1,10 +1,35 @@
+// import type { NextConfig } from "next";
+
+// const nextConfig: NextConfig = {
+// images: {
+//     domains: [
+//       "api.ppepca.com"
+//     ],
+//   },};
+
+// export default nextConfig;
+
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-images: {
-    domains: [
-      "api.ppepca.com"
-    ],
-  },};
+  images: {
+    domains: ["api.ppepca.com"],
+  },
+  async redirects() {
+    return [
+      {
+        source: '/aboutppepca.html', // old URL
+        destination: '/about',       // new route
+        permanent: true,             // 301 redirect
+      },
+      // You can add more redirects here
+      // {
+      //   source: '/contactppepca.html',
+      //   destination: '/contact',
+      //   permanent: true,
+      // },
+    ];
+  },
+};
 
 export default nextConfig;
