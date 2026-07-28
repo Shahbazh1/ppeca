5f65f63fb0a92ab0064fd4307d0a4c16b1e02882
Shahbazh1 <shahbazhashmi235@gmail.com>
2026-07-28 14:24:50 +0500

    add the election section in the news page

--- ppeca/src/app/news/page.tsx
+++ ppeca/src/app/news/page.tsx
@@ -2,6 +2,7 @@ import LatestNewsHero from "../../components/news/LatestNewsHero";
 import NewsLetters from "../../components/news/NewsLetters";
 import InveryData from "../../components/news/InveryData";
 import DirectoryOfTrainingProviders from "../../components/news/DirectoryOfTrainingProviders";
+import PpepcaElections from "../../components/news/PpepcaElections";
 import Head from "next/head";
 export default function Page() {
   return (
@@ -49,6 +50,7 @@ export default function Page() {
 
       <LatestNewsHero />
       <div className="xl:px-[4.875rem] lg:px-[4.3875rem] md:px-[3.9rem] sm:px-[3.4125rem] px-[2.925rem] bg-[#f8fafc]">
+        <PpepcaElections/>
         <NewsLetters />
         <InveryData />
         <DirectoryOfTrainingProviders />
